b379f4bf809d59b7003ee105a41587b59fabadb0
Tyler Benton <tjbenton21@gmail.com>
2015-09-17 09:15:26 -0400

    Moved the theme styles to be after the highlight js styles

--- index.jsx
+++ index.jsx
@@ -13,10 +13,10 @@ import Deck from "./presentation/deck";
 import config from "./presentation/config";
 
 // compiles the scss for the page inserts it on change
-require("!style!css!sass!./themes/sassinate/scss/index.scss");
 require("!style!css!highlight.js/styles/color-brewer.css");
 // tomorrow
 // color-brewer
+require("!style!css!sass!./themes/sassinate/scss/index.scss");
 
 const flux = new Flux();
 Alt.debug("flux", flux);
