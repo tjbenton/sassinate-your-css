f62b332479884972f94cb891ea3c2f343a4e357a
Tyler Benton <tjbenton21@gmail.com>
2015-09-20 10:26:26 -0400

    Reworked Note to use Step instead of duplicating code

--- src/note.jsx
+++ src/note.jsx
@@ -1,51 +1,24 @@
 import React from "react/addons";
-import {StepMixin} from "./step";
 
-const Note = React.createClass({
-  mixins: [StepMixin],
+export default class Note extends React.Component{
   render(){
-    let classes = ["c-notes__note"],
-        {
-          className,
-          isVisible,
-          isInvisible,
-          animateIn,
-          animateOut,
-          ...rest
-        } = this.props;
-
-    // adds any passed classes
-    className && classes.push(...className.split(" "));
-
-    // handles adding the active classes
-    if(this.state.visible){
-      classes.push(...[
-        "is-visible",
-        ...(isVisible || "").split(" "),
-        animateIn ? `u-animate u-animate--${animateIn}` : ""
-      ]);
+    if(this.props.order){
+      return (
+        <li className="c-notes__note" ref="note">
+          <Step order={this.props.order} className={`c-step--highlight is-visible ${this.props.className}`}>
+            {this.props.children}
+          </Step>
+        </li>
+      );
     }
-
-    // handles the exiting classes
-    if(!this.state.visible && this.state.was_active){
-      classes.push(...[
-        "is-visible",
-        ...(isInvisible || "").split(" "),
-        animateOut ? `u-animate u-animate--${animateOut}` : ""
-      ]);
+    else{
+      return (
+        <li className="c-notes__note">
+          {this.props.children}
+        </li>
+      );
     }
-
-    // this will set `is-prev`, `is-current`, `is-next`
-    if(!!this.state.type){
-      classes.push(`is-${this.state.type}`);
-    }
-
-    return (
-      <li className={classes.filter(Boolean).join(" ")} ref="note" {...rest}>
-        {this.props.children}
-      </li>
-    );
   }
-});
+};
 
 export default Note;
